980c12e81f6c95826930c21bdd2b7ab778fda796
Igor Batista Lima <112262600+IgorBatistaLima@users.noreply.github.com>
2024-09-10 20:12:38 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,14 +17,15 @@ const allowedOrigins = ['http://localhost:5173', 'https://server-project-orcin.v
 
 
 const corsOptions = {
-    origin: function (origin, callback) {
-
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 
