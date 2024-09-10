742e52fe1775d04f6d0d59ccbb50e742901971a2
Igor Batista Lima <112262600+IgorBatistaLima@users.noreply.github.com>
2024-09-10 13:35:48 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const conect = require('./config/db');
+const helmet = require('helmet');
+const connect = require('./config/db');
 const gameRoutes = require('./routes/gameRoutes');
 const userRoutes = require('./routes/userRoutes');
 const installgameRoutes = require('./routes/install-games-Routes');
@@ -8,15 +9,33 @@ require('dotenv').config();
 const app = express();
 const cors = require('cors');
 
-conect();
-app.use(cors());
+
+connect();
+
+
+const allowedOrigins = ['http://localhost:5173', 'https://server-project-orcin.vercel.app/'];
+
+
+const corsOptions = {
+    origin: function (origin, callback) {
+
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
+app.use(helmet());
 
 app.use('/api/games', gameRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/install-games', installgameRoutes);
 app.use('/api/install-users', installusersRoutes);
-
-
 
 module.exports = app;
