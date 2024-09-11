bfd785f5642331226fc2345d2db6c8c7ad6f4239
Igor Batista Lima <112262600+IgorBatistaLima@users.noreply.github.com>
2024-09-11 00:57:19 -0300

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,13 +5,12 @@ const { check, validationResult } = require('express-validator');
 const logger = require('../config/logger');
 require('dotenv').config();
 
-// Função para obter o IP do cliente
 const getClientIp = (req) => {
     return req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.ip;
 };
 
 const register = async (req, res, next) => {
-    const { username, email, password, role } = req.body;
+    const { username, email, password } = req.body;
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
@@ -19,21 +18,18 @@ const register = async (req, res, next) => {
         await user.save();
         res.json({ message: 'Registration successful' });
     } catch (error) {
-        logger.error(`Registration failed: ${error.message} - IP: ${getClientIp(req)} - Method: ${req.method}`); // Registro de erros com detalhes
+        logger.error(`Registration failed: ${error.message} - IP: ${getClientIp(req)} - Method: ${req.method}`);
         next(error);
     }
 };
 
-// Função para login de usuário
 const login = [
-    // Validação de entrada
     check('username').notEmpty().withMessage('Username is required'),
     check('password').notEmpty().withMessage('Password is required'),
 
     async (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            // Envio de mensagens de validação do servidor
             return res.status(400).json({ errors: errors.array() });
         }
 
@@ -43,22 +39,22 @@ const login = [
             const user = await User.findOne({ username });
 
             if (!user) {
-                logger.error(`User not found - IP: ${getClientIp(req)} - Method: ${req.method}`); // Registro de erros com detalhes
+                logger.error(`User not found - IP: ${getClientIp(req)} - Method: ${req.method}`);
                 return res.status(404).json({ message: 'User not found' });
             }
 
             const passwordMatch = await bcrypt.compare(password, user.password);
             if (!passwordMatch) {
-                logger.error(`Incorrect password - IP: ${getClientIp(req)} - Method: ${req.method}`); // Registro de erros com detalhes
+                logger.error(`Incorrect password - IP: ${getClientIp(req)} - Method: ${req.method}`);
                 return res.status(401).json({ message: 'Incorrect password' });
             }
 
-            const payload = { _id: user._id, role: user.role };
+            const payload = { _id: user._id };
             const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '24h' });
 
             res.json({ token });
         } catch (error) {
-            logger.error(`Login failed: ${error.message} - IP: ${getClientIp(req)} - Method: ${req.method}`); // Registro de erros com detalhes
+            logger.error(`Login failed: ${error.message} - IP: ${getClientIp(req)} - Method: ${req.method}`);
             next(error);
         }
     }
