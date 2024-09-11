3680ae2beccee3438d887fe715fdd82f99fc8a4a
Igor Batista Lima <112262600+IgorBatistaLima@users.noreply.github.com>
2024-09-11 00:56:24 -0300

    Update gameRoutes.js

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -2,12 +2,31 @@ const express = require('express');
 const { getGames, getGame, addGame, updateGame, deleteGame } = require('../controllers/gameController');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
+const checkCache = require('../middlewares/cache');
+const NodeCache = require("node-cache");
+const myCache = new NodeCache();
 
-router.get('/games', authMiddleware, getGames);
-router.get('/games/:id', getGame);
-router.post('/games', addGame);
-router.put('/games/:id', updateGame);
-router.delete('/games/:id', deleteGame);
+router.get('/games', authMiddleware, checkCache, getGames);
+router.get('/games/:id', checkCache, getGame);
 
+router.post('/games', (req, res, next) => {
+    myCache.del('/games');
+    addGame(req, res, next).then(() => {
+    }).catch(next);
+});
+
+router.put('/games/:id', (req, res, next) => {
+    myCache.del('/games');
+    myCache.del(`/games/${req.params.id}`);
+    updateGame(req, res, next).then(() => {
+    }).catch(next);
+});
+
+router.delete('/games/:id', (req, res, next) => {
+    myCache.del('/games');
+    myCache.del(`/games/${req.params.id}`);
+    deleteGame(req, res, next).then(() => {
+    }).catch(next);
+});
 
 module.exports = router;
